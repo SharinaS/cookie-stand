9f72eae41698689d174f4fce348eb726fbc4ddd7
Sharina Stubbs <sharinastubbs@gmail.com>
2019-07-16 17:32:55 -0700

    Built a prototype function that populates a final array. Built a makeHeader function that populates the headers with hours

--- js/app.js
+++ js/app.js
@@ -1,12 +1,9 @@
 'use strict';
 
-// Ways to organize code further:
-// create another function within each object that adds literal strings (ie, `${hours[i]}: ${this.cookiesEachHour[i]} cookies) instead of having strings within each object
-// var ulEl = document.getElementById('cookiesNeededSeatac'); <---- this is a global variable that can be referenced instead of having it inside each object
-
 // array of hours
 var time = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 var allLocations = [];
+var tableEl = document.getElementById('table');
 
 // constructor function for business locations
 function BusinessLocation(name, minPerCust, maxPerCust, avgCookiePerSale) {
@@ -15,8 +12,9 @@ function BusinessLocation(name, minPerCust, maxPerCust, avgCookiePerSale) {
   this.max = maxPerCust;
   this.cookieAvg = avgCookiePerSale;
   this.avgCustomerNum = [];
-  this.cookieNumPerHr = []; 
-  this.totalCookies = 
+  this.cookieNumPerHr = [];
+  this.totalCookies = 0;
+  this.finalArray = [];
   allLocations.push(this);
 }
 
@@ -35,25 +33,25 @@ BusinessLocation.prototype.cookiesPerHour = function(){
   this.customerNum();
   for (var i = 0; i < time.length; i++) {
     var cookieValue = Math.ceil(this.cookieAvg * this.avgCustomerNum[i]);
-    console.log('cookie value', cookieValue);
+    //console.log('cookie value', cookieValue);
     this.cookieNumPerHr.push(cookieValue);
     //add up cookies
     this.totalCookies += this.cookieNumPerHr[i];
   }
 };
 
-
-/*
-render: function(){
-    var ulEl = document.getElementById('cookiesNeededPike');
-    for (var i = 0; i < time.length; i++) {
-      var liEl = document.createElement('li');
-      liEl.textContent = time[i] + ': ' + this.cookieNumPerHr[i] + ' cookies';
-      ulEl.appendChild(liEl);
-    }
-    liEl.textContent = `Total: ${this.totalCookies} cookies`;
+BusinessLocation.prototype.finalArrayMadePretty = function(){
+  this.cookiesPerHour();
+  this.finalArray.push(this.name);
+  for (var i = 0; i < time.length; i++) {
+    this.finalArray.push(this.cookieNumPerHr[i]);  // <---------- is it populating the correct size array?
   }
-*/
+  this.finalArray.push(this.totalCookies);
+};
+
+BusinessLocation.prototype.renderAll = function(){
+  this.finalArrayMadePretty; // <------- is this where this goes?
+};
 
 
 // make instances
@@ -64,11 +62,6 @@ new BusinessLocation('Capitol Hill', 20, 38, 2.3);
 new BusinessLocation('Alki', 2, 16, 4.6);
 
 
-// loop through each instance and for each instance, call the method on the prototype to make it run
-for (var i = 0; i < allLocations.length; i++){  
-  allLocations[i].customerNum();
-  console.log('for loop that iterates through each instance');
-}
 
 // helper function to generate random number of customers
 // got this function from MDN - math.random() doc
@@ -76,219 +69,25 @@ function makesRandomNumber(min, max){
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-/*
+// function to make header for table:
+function makeHeader(){
+  var trEl = document.createElement('tr');
+  tableEl.appendChild(trEl);
 
-// object literals for each store
-var FirstAndPike = {
-  minPerCust: 23,
-  maxPerCust: 65,
-  avgCookiePerSale: 6.3,
-  customerNum: [],
-  cookieNumPerHr: [],
-  totalCookies: 0,
-  // generate the number of customers for each hour
-  getCustomerNum: function() {
-    for (var i = 0; i < time.length; i++) {
-      var customerValue = randomCustPerHour(this.minPerCust, this.maxPerCust);
-      this.customerNum.push(customerValue);
-    }
-    console.log('customer number generated: ' + this.customerNum);
-  },
-  // generate the number of cookies
-  getCookiesPerHour: function() {
-    this.getCustomerNum();
-    for (var i = 0; i < this.customerNum.length; i++) {
-      var cookieValue = Math.ceil(this.avgCookiePerSale * this.customerNum[i]);
-      this.cookieNumPerHr.push(cookieValue);
-      //add up cookies
-      this.totalCookies += this.cookieNumPerHr[i];
-    }
-    console.log('cookies per hour is: ' + this.cookieNumPerHr);
-    console.log('total cookies for the day is: ' + this.totalCookies);
-  },
-  render: function(){
-    var ulEl = document.getElementById('cookiesNeededPike');
-    for (var i = 0; i < time.length; i++) {
-      var liEl = document.createElement('li');
-      liEl.textContent = time[i] + ': ' + this.cookieNumPerHr[i] + ' cookies';
-      ulEl.appendChild(liEl);
-    }
-    liEl.textContent = `Total: ${this.totalCookies} cookies`;
+  for(var i = 0; i < time.length; i++){
+    var thEl = document.createElement('th');
+    thEl.textContent = time[i];
+    trEl.appendChild(thEl);
   }
-
-}; // end of FirstAndPike object
-
-
-var SeaTacAirport = {
-  minPerCust: 3,
-  maxPerCust: 24,
-  avgCookiePerSale: 1.2,
-  customerNum: [],
-  cookieNumPerHr: [],
-  totalCookies: 0,
-  getCustomerNum: function() {
-    for (var i = 0; i < time.length; i++) {
-      var customerValue = randomCustPerHour (this.minPerCust, this.maxPerCust);
-      this.customerNum.push(customerValue);
-    }
-    console.log('customer number generated: ' + this.customerNum);
-  },
-  getCookiesPerHour: function() {
-    this.getCustomerNum();
-    for (var i = 0; i < this.customerNum.length; i++) {
-      var cookieValue = Math.ceil(this.avgCookiePerSale * this.customerNum[i]);
-      this.cookieNumPerHr.push(cookieValue);
-      //add up cookies
-      this.totalCookies += this.cookieNumPerHr[i];
-    }
-    console.log('cookies per hour is: ' + this.cookieNumPerHr);
-    console.log('total cookies for the day is: ' + this.totalCookies);
-  },
-  render: function(){
-    var ulEl = document.getElementById('cookiesNeededSeatac');
-    for (var i = 0; i < time.length; i++) {
-      var liEl = document.createElement('li');
-      liEl.textContent = time[i] + ': ' + this.cookieNumPerHr[i] + ' cookies';
-      ulEl.appendChild(liEl);
-    }
-    liEl.textContent = `Total: ${this.totalCookies} cookies`;
-  }
-}; // end of SeaTac object
-
-
-var seattleCenter = {
-  minPerCust: 23,
-  maxPerCust: 65,
-  avgCookiePerSale: 6.3,
-  customerNum: [],
-  cookieNumPerHr: [],
-  totalCookies: 0,
-  getCustomerNum: function() {
-    for (var i = 0; i < time.length; i++) {
-      // function randomCustPerHour is called every iteration and pushed to
-      var customerValue = randomCustPerHour (this.minPerCust, this.maxPerCust);
-      this.customerNum.push(customerValue);
-    }
-    console.log('customer number generated: ' + this.customerNum);
-  },
-  getCookiesPerHour: function() {
-    this.getCustomerNum();
-    for (var i = 0; i < this.customerNum.length; i++) {
-      var cookieValue = Math.ceil(this.avgCookiePerSale * this.customerNum[i]);
-      this.cookieNumPerHr.push(cookieValue);
-      //add up cookies
-      this.totalCookies += this.cookieNumPerHr[i];
-    }
-    console.log('cookies per hour is: ' + this.cookieNumPerHr);
-    console.log('total cookies for the day is: ' + this.totalCookies);
-  },
-  render: function(){
-    var ulEl = document.getElementById('cookiesNeededCenter');
-    for (var i = 0; i < time.length; i++) {
-      var liEl = document.createElement('li');
-      liEl.textContent = time[i] + ': ' + this.cookieNumPerHr[i] + ' cookies';
-      ulEl.appendChild(liEl);
-    }
-    liEl.textContent = `Total: ${this.totalCookies} cookies`;
-  }
-}; // end of Seattle Center object
-
-
-var capitolHill = {
-  minPerCust: 23, 
-  maxPerCust: 65,
-  avgCookiePerSale: 6.3,
-  customerNum: [],
-  cookieNumPerHr: [],
-  totalCookies: 0,
-  getCustomerNum: function() {
-    for (var i = 0; i < time.length; i++) {
-      var customerValue = randomCustPerHour (this.minPerCust, this.maxPerCust);
-      this.customerNum.push(customerValue);
-    }
-    console.log('customer number generated: ' + this.customerNum);
-  },
-  getCookiesPerHour: function() {
-    this.getCustomerNum();
-    for (var i = 0; i < this.customerNum.length; i++) {
-      var cookieValue = Math.ceil(this.avgCookiePerSale * this.customerNum[i]);
-      this.cookieNumPerHr.push(cookieValue);
-      this.totalCookies += this.cookieNumPerHr[i];
-    }
-    console.log('cookies per hour is: ' + this.cookieNumPerHr);
-    console.log('total cookies for the day is: ' + this.totalCookies);
-  },
-  render: function(){
-    var ulEl = document.getElementById('cookiesNeededHill');
-    for (var i = 0; i < time.length; i++) {
-      var liEl = document.createElement('li');
-      liEl.textContent = time[i] + ': ' + this.cookieNumPerHr[i] + ' cookies';
-      ulEl.appendChild(liEl);
-    }
-    liEl.textContent = `Total: ${this.totalCookies} cookies`;
-  }
-}; // end of Capitol Hill object
-
-
-var alki = {
-  minPerCust: 23, 
-  maxPerCust: 65,
-  avgCookiePerSale: 6.3,
-  customerNum: [],
-  cookieNumPerHr: [],
-  totalCookies: 0,
-  getCustomerNum: function() {
-    for (var i = 0; i < time.length; i++) {
-      var customerValue = randomCustPerHour (this.minPerCust, this.maxPerCust);
-      this.customerNum.push(customerValue);
-    }
-    console.log('customer number generated: ' + this.customerNum);
-  },
-  getCookiesPerHour: function() {
-    this.getCustomerNum();
-    for (var i = 0; i < this.customerNum.length; i++) {
-      var cookieValue = Math.ceil(this.avgCookiePerSale * this.customerNum[i]);
-      this.cookieNumPerHr.push(cookieValue);
-      //add up cookies
-      this.totalCookies += this.cookieNumPerHr[i];
-    }
-    console.log('cookies per hour is: ' + this.cookieNumPerHr);
-    console.log('total cookies for the day is: ' + this.totalCookies);
-  },
-  render: function(){
-    var ulEl = document.getElementById('cookiesNeededAlki');
-    for (var i = 0; i < time.length; i++) {
-      var liEl = document.createElement('li');
-      liEl.textContent = time[i] + ': ' + this.cookieNumPerHr[i] + ' cookies';
-      ulEl.appendChild(liEl);
-    }
-    liEl.textContent = `Total: ${this.totalCookies} cookies`;
-  }
-}; // end of Capitol Hill object
-
-
-// --> Call the Functions <--
-//First and Pike
-FirstAndPike.getCookiesPerHour();
-FirstAndPike.render();
-// SeaTac
-SeaTacAirport.getCookiesPerHour();
-SeaTacAirport.render();
-//Seattle Center
-seattleCenter.getCookiesPerHour();
-seattleCenter.render();
-//Capitol Hill
-capitolHill.getCookiesPerHour();
-capitolHill.render();
-//Alki
-alki.getCookiesPerHour();
-alki.render();
-
-
-// helper function to generate random number of customers
-// got this function from MDN - math.random() doc
-function randomCustPerHour(min, max){
-  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-*/
+makeHeader();
+
+// loop through each instance and for each instance, call the method on the prototype to make it run
+for (var i = 0; i < allLocations.length; i++){
+  //allLocations[i].customerNum();
+  allLocations[i].renderAll();  // <--------- is this needed after it all done?
+  //console.log('for loop that iterates through each instance');
+}
+
+
